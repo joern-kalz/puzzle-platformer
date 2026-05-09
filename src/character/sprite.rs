a6d569f1745ef92865e573a25065a8e89c3d9ebd
joern-kalz <joern.kalz@gmail.com>
2026-05-09 16:35:03 +0200

    refactor: Extract sprite

--- src/character/sprite.rs
+++ src/character/sprite.rs
@@ -5,15 +5,29 @@ pub const COLLIDER_MARGIN_X: i32 = 17;
 pub const COLLIDER_TOP: i32 = 13;
 pub const COLLIDER_BOTTOM: i32 = 49;
 
-pub fn is_on_ground(x: i32, y: i32, screen: &Screen) -> bool {
-    let start_x = x + COLLIDER_MARGIN_X;
-    let end_x = x + SPRITE_WIDTH - COLLIDER_MARGIN_X;
-    let y = y + COLLIDER_BOTTOM;
+#[derive(Clone, Copy)]
+pub enum Direction {
+    Left = -1,
+    Right = 1,
+}
 
-    for x in start_x..=end_x {
-        if screen.get_pixel(x, y)[3] > 0 {
-            return true;
+pub struct Sprite {
+    pub x: i32,
+    pub y: i32,
+    pub direction: Direction,
+}
+
+impl Sprite {
+    pub fn is_on_ground(&self, screen: &Screen) -> bool {
+        let start_x = self.x + COLLIDER_MARGIN_X;
+        let end_x = self.x + SPRITE_WIDTH - COLLIDER_MARGIN_X;
+        let y = self.y + COLLIDER_BOTTOM;
+
+        for x in start_x..=end_x {
+            if screen.get_pixel(x, y)[3] > 0 {
+                return true;
+            }
         }
+        false
     }
-    false
 }
--- src/character/walking.rs
+++ src/character/walking.rs
@@ -1,5 +1,5 @@
 use crate::character::sprite::{
-    is_on_ground, COLLIDER_BOTTOM, COLLIDER_MARGIN_X, COLLIDER_TOP, SPRITE_WIDTH,
+    Direction, Sprite, COLLIDER_BOTTOM, COLLIDER_MARGIN_X, COLLIDER_TOP, SPRITE_WIDTH,
 };
 use crate::character::state::State;
 use crate::screen::{DrawParams, FrameSet, Screen};
@@ -7,37 +7,31 @@ use crate::screen::{DrawParams, FrameSet, Screen};
 const NUM_SPRITES: i32 = 6;
 const MAX_STEP_HEIGHT: i32 = 10;
 
-#[derive(Clone, Copy)]
-pub enum Direction {
-    Left = -1,
-    Right = 1,
-}
-
 pub struct Walking {
-    x: i32,
-    y: i32,
+    sprite: Sprite,
     sprite_index: i32,
-    direction: Direction,
 }
 
 impl Walking {
     pub fn new(x: i32, y: i32) -> Self {
         Walking {
-            x,
-            y,
+            sprite: Sprite {
+                x,
+                y,
+                direction: Direction::Right,
+            },
             sprite_index: 0,
-            direction: Direction::Right,
         }
     }
 
     fn is_colliding_with_wall(&self, screen: &Screen, offset: i32) -> bool {
-        let x = match self.direction {
-            Direction::Right => self.x + SPRITE_WIDTH - COLLIDER_MARGIN_X,
-            Direction::Left => self.x + COLLIDER_MARGIN_X,
+        let x = match self.sprite.direction {
+            Direction::Right => self.sprite.x + SPRITE_WIDTH - COLLIDER_MARGIN_X,
+            Direction::Left => self.sprite.x + COLLIDER_MARGIN_X,
         };
 
         for y in COLLIDER_TOP..COLLIDER_BOTTOM {
-            if screen.get_pixel(x, y + self.y - offset)[3] > 0 {
+            if screen.get_pixel(x, y + self.sprite.y - offset)[3] > 0 {
                 return true;
             }
         }
@@ -50,20 +44,20 @@ impl State for Walking {
     fn update(&mut self, screen: &Screen, time: f64) -> Option<Box<dyn State>> {
         self.sprite_index = (time / 100.0) as i32 % NUM_SPRITES;
 
-        if !is_on_ground(self.x, self.y, screen) {
-            self.y += 1;
+        if !self.sprite.is_on_ground(screen) {
+            self.sprite.y += 1;
             return None;
         }
 
         for offset in 0..MAX_STEP_HEIGHT {
             if !self.is_colliding_with_wall(screen, offset) {
-                self.y -= offset;
-                self.x += self.direction as i32;
+                self.sprite.y -= offset;
+                self.sprite.x += self.sprite.direction as i32;
                 return None;
             }
         }
 
-        self.direction = match self.direction {
+        self.sprite.direction = match self.sprite.direction {
             Direction::Left => Direction::Right,
             Direction::Right => Direction::Left,
         };
@@ -72,11 +66,11 @@ impl State for Walking {
 
     fn draw(&self, screen: &mut Screen) {
         screen.draw(DrawParams {
-            x: self.x,
-            y: self.y,
+            x: self.sprite.x,
+            y: self.sprite.y,
             frame_set: FrameSet::Walking,
             frame_index: self.sprite_index,
-            mirror_x: matches!(self.direction, Direction::Left),
+            mirror_x: matches!(self.sprite.direction, Direction::Left),
             mirror_y: false,
         });
     }
